3eece82f61034f20c1a933527448f7a5002db960
Shubham Jain <NH1977@nslhub.in>
2024-03-14 00:33:29 +0530

    changs

---
+++ lib/BasicContainer.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class BasicContainer extends StatelessWidget {
+  BasicContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', height: 300, width: 400),
+          const Padding(padding: EdgeInsets.only(top: 80)),
+          const Text('Lets check our Compatibility',
+              style: TextStyle(fontSize: 24, color: Colors.white)),
+          const Padding(padding: EdgeInsets.only(top: 30)),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: const Color.fromARGB(255, 67, 5, 78),
+            ),
+            child: const Text(
+              'Start Quiz',
+              style: TextStyle(fontSize: 15, color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:compatibilty_quiz/BasicContainer.dart';
+
+
+void  main() {
+  runApp(
+    MaterialApp(
+     home: Scaffold(
+      backgroundColor:  const Color.fromARGB(255, 74, 3, 87),
+      body:  BasicContainer() 
+      ),
+      ),
+  );
+}
